4c28b8117babbd41ce51b21a7101863c47956842
Jas <55251065+jaswpratt@users.noreply.github.com>
2019-12-06 22:49:30 -0500

    Initial commit.

---
+++ src/va/loudoun/leesburg/controller/TriviaServlet.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package va.loudoun.leesburg.controller;
+
+/**
+ * @author Sabine
+ *
+ */
+public class TriviaServlet {
+
+	/**
+	 * 
+	 */
+	public TriviaServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/va/loudoun/leesburg/dao/TriviaSourceDAO.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package va.loudoun.leesburg.dao;
+
+/**
+ * @author Sabine
+ *
+ */
+public class TriviaSourceDAO {
+
+	/**
+	 * 
+	 */
+	public TriviaSourceDAO() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
