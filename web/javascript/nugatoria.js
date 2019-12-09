1e434e32b5ee7c3ae73858f90baa48ca1c06c7e2
Jas <55251065+jaswpratt@users.noreply.github.com>
2019-12-09 12:18:03 -0500

    Update nugatoria.js

--- web/javascript/nugatoria.js
+++ web/javascript/nugatoria.js
@@ -1,3 +1,14 @@
 /**
  * nugatoria.js
+ * https://www.tutorialsteacher.com/d3js/loading-data-from-file-in-d3js
  */
+
+
+function loadQuestionParams(){
+  d3.json("json/questionData.json", function(data) {
+      for (var i = 0; i < data.length; i++) {
+          console.log(data);
+          console.log(data);
+      }
+  });
+}
