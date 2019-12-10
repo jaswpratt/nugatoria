fc97c718ce62865c4ebd58e1cda841ea1718ddbc
Jas <55251065+jaswpratt@users.noreply.github.com>
2019-12-10 11:01:13 -0500

    Update TriviaSourceDAO.java

--- src/va/loudoun/leesburg/dao/TriviaSourceDAO.java
+++ src/va/loudoun/leesburg/dao/TriviaSourceDAO.java
@@ -13,13 +13,20 @@ import va.loudoun.leesburg.model.TriviaQuestion;
  *
  */
 public class TriviaSourceDAO {
-
+        public static final String opentdbUrl = "https://opentdb.com/api.php?";
 	/**
 	 * 
 	 */
 	public TriviaSourceDAO() { }
 
-	public List getTriviaQuestions() {
+	public List getTriviaQuestions(String count, String category, String type, String difficulty) {
+		StringBuffer otdbUrlGetRequest = new StringBuffer();
+		otdbUrlGetRequest.append(opentdbUrl);
+		otdbUrlGetRequest.append("?amount=" + count);
+		otdbUrlGetRequest.append("&category=" + category);
+		otdbUrlGetRequest.append("&difficulty=" + difficulty);
+		otdbUrlGetRequest.append("&type=" + type);&encode=url3986
+	        otdbUrlGetRequest.append("&encode=url3986");
 		List triviaQuestionsList = new ArrayList<TriviaQuestion>();
 		TriviaQuestion triviaQuestion = new TriviaQuestion();
 		
