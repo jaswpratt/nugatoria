fa2c497ea815f948705c311ebca9f268bc41066b
Jas <55251065+jaswpratt@users.noreply.github.com>
2019-12-10 08:42:33 -0500

    Update nugatoria.js
    Added onSelect function handler for comboboxes.

--- web/javascript/nugatoria.js
+++ web/javascript/nugatoria.js
@@ -3,27 +3,47 @@
  * https://www.tutorialsteacher.com/d3js/loading-data-from-file-in-d3js
  * https://www.w3schools.com/js/js_json_files.asp
  */
+   var count {id : "10", value: "10"};
+   var category {id : "any",value: ""};
+   var type {id : "any", value: ""};
+   var difficulty {id : "any", value: ""};
 
    $( document ).ready(function() {
       $('#count').combobox({
          url:'json/count.json',
          valueField:'id',
-         textField:'count'
+         textField:'count',
+         onSelect: function(rec){
+            count.id = rec.id;
+            count.value = rec.count;
+         }
       });
       $('#category').combobox({
          url:'json/categoryData.json',
          valueField:'id',
-         textField:'category'
+         textField:'category',
+         onSelect: function(rec){
+            category.id = rec.id;
+            category.value = rec.category;
+         }
       });
       $('#type').combobox({
           url:'json/type.json',
           valueField:'id',
-          textField:'category'
+          textField:'type',
+          onSelect: function(rec){
+             type.id = rec.id;
+             type.value = rec.type;
+          }
        });
       $('#difficulty').combobox({
           url:'json/difficulty.json',
           valueField:'id',
-          textField:'category'
+          textField:'difficulty',
+          onSelect: function(rec){
+             difficulty.id = rec.id;
+             difficulty.value = rec.difficulty;
+          }
        });
    });
 
